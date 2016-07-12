4a9e3cb2301bdefae9a1418329b32c132ded26b9
JamesVanWaza <jgacuca@gmail.com>
2016-07-12 14:33:46 -0400

    Added the Kenshopping Theme

---
+++ wp-content/themes/kenshopping/footer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ *
+ * @package kenshopping
+ */
+?>
+<i class="fa fa-copyright"></i> DOSBRANDING <?php echo date(); ?>
+<?php wp_footer();?>
