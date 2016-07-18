7dab7bc221cfd6ea4b357e990448ac058d0ab6b8
James Van Waza <jgacuca@gmail.com>
2016-07-18 07:52:09 -0400

    Made the Footer Sticky Bottom

--- wp-content/themes/kenshopping/footer.php
+++ wp-content/themes/kenshopping/footer.php
@@ -6,6 +6,8 @@
  * @package kenshopping
  */
 ?>
-2015 - 2016 Kenafrica
-<i class="fa fa-copyright"></i> DOSBRANDING <?php echo the_date(); ?>
+<footer class="navbar navbar-default navbar-fixed-bottom">
+  2015 - 2016 Kenafrica
+  <i class="fa fa-copyright"></i> DOSBRANDING <?php echo the_date(); ?>
+</footer>
 <?php wp_footer();?>
