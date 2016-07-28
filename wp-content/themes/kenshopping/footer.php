fcf2405db66a2948a3c8c68f845dea1501c3dd9d
James Van Waza <jgacuca@gmail.com>
2016-07-28 12:11:22 -0400

    Changed the footer layout

--- wp-content/themes/kenshopping/footer.php
+++ wp-content/themes/kenshopping/footer.php
@@ -7,8 +7,8 @@
  */
 ?>
 <footer class="navbar navbar-default navbar-fixed-bottom text-center">
-  2015 - 2016 Kenafrica
+  <!--2015 - 2016 Kenafrica Add using PHP-->
   <br>
-    <i class="fa fa-copyright"></i> Designed by <a href="https://dosbranding.com">DOSBRANDING</a> <?php echo the_date(); ?>
+    <i class="fa fa-copyright"></i> Designed by <a href="https://dosbranding.com">DOSBRANDING</a> <?php echo the_date('Y'); ?>
 </footer>
 <?php wp_footer();?>
