e6d6fa12cc381f7673c09bd7b28522ebef765388
James Van Waza <jgacuca@gmail.com>
2016-08-17 15:02:38 -0400

    Updated the navbar

--- wp-content/themes/kenshopping/header.php
+++ wp-content/themes/kenshopping/header.php
@@ -9,13 +9,17 @@
  * @package kenshopping
  */
 
-?><!DOCTYPE html>
-<html <?php language_attributes();?>>
-<head>
-<meta charset="<?php bloginfo('charset');?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo('pingback_url');?>">
+?>
+<!DOCTYPE html>
+	<html <?php language_attributes();?>>
+	<head>
+	<meta charset="<?php bloginfo('charset');?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo('pingback_url');?>">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon@2x.png" />
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon@3x.png" />
 
 <?php wp_head();?>
 </head>
@@ -23,19 +27,19 @@
 <body <?php body_class();?>>
 	<nav  class="navbar navbar-default">
 		<ul class="nav navbar-nav"></ul>
-		<?php
-				 wp_nav_menu( array(
-						 'menu'              => 'primary',
-						 'theme_location'    => 'primary',
-						 'depth'             => 2,
-						 'container'         => 'div',
-						 'container_class'   => 'collapse navbar-collapse',
-		 			 	 'container_id'      => 'collapsenavbar',
-						 'menu_class'        => 'nav navbar-nav',
-						 'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-						 'walker'            => new wp_bootstrap_navwalker())
-				 );
-		 ?>
+<?php
+wp_nav_menu(array(
+	'menu' => 'primary',
+	'theme_location' => 'primary',
+	'depth' => 2,
+	'container' => 'div',
+	'container_class' => 'collapse navbar-collapse',
+	'container_id' => 'collapsenavbar',
+	'menu_class' => 'nav navbar-nav',
+	'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+	'walker' => new wp_bootstrap_navwalker())
+);
+?>
 		</ul>
 		<button type="button" name="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsenavbar" aria-expanded="false"></button>
 		<span class="icon-bar"></span>
