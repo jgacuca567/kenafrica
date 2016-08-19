021b8d16fe79555df61af3a486f3b2c3603e8e21
James Van Waza <jgacuca@gmail.com>
2016-08-19 09:32:21 -0400

    Added Bootstrap JS

--- wp-content/themes/kenshopping/inc/enqueue-scripts.php
+++ wp-content/themes/kenshopping/inc/enqueue-scripts.php
@@ -2,29 +2,32 @@
 /**
  * Enqueue scripts and styles.
  */
-function kenshopping_scripts()
-{
-    wp_enqueue_style('kenshopping-style', get_stylesheet_uri());
+function kenshopping_scripts() {
+	wp_enqueue_style('kenshopping-style', get_stylesheet_uri());
 
-    /** Font Awesome 4.6.3 */
-    wp_register_style('fontawesome', get_stylesheet_directory_uri() . '/node_modules/font-awesome/css/font-awesome.css', '4.6.3');
+	/** Font Awesome 4.6.3 */
+	wp_register_style('fontawesome', get_stylesheet_directory_uri() . '/node_modules/font-awesome/css/font-awesome.css', '4.6.3');
 
-    /** Deregister the jquery version bundled with wordpress */
-    wp_deregister_script('jquery');
+	/** Deregister the jquery version bundled with wordpress */
+	wp_deregister_script('jquery');
 
-    /** Local Version of jQuery */
-    wp_register_script('jquery', get_template_directory_uri() . '/node_modules/jquery/dist/jquery.min.js', array(), '3.1.0');
+	/** Local Version of jQuery */
+	wp_register_script('jquery', get_template_directory_uri() . '/node_modules/jquery/dist/jquery.min.js', array(), '3.1.0');
 
-    /** Script.JS */
-    wp_register_script('scriptjs', get_template_directory_uri() . '/js/script.js');
+	/** Bootstrap JS */
+	wp_register_script('bootstrapjs', get_template_directory_uri() . '/node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js', array('jquery'), '3.3.7');
 
-    /** Enqueue all registered scripts */
-    wp_enqueue_style('fontawesome');
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('scriptjs');
+	/** Script.JS */
+	wp_register_script('scriptjs', get_template_directory_uri() . '/js/script.js');
 
-    if (is_singular() && comments_open() && get_option('thread_comments')) {
-        wp_enqueue_script('comment-reply');
-    }
+	/** Enqueue all registered scripts */
+	wp_enqueue_style('fontawesome');
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('scriptjs');
+	wp_enqueue_script('bootstrapjs');
+
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
+	}
 }
 add_action('wp_enqueue_scripts', 'kenshopping_scripts');
