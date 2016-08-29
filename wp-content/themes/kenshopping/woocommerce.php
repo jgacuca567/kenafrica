51ebef127543875013271f70e3e79a21f58da975
James Van Waza <jgacuca@gmail.com>
2016-08-29 14:16:05 -0400

    Added the section class page

--- wp-content/themes/kenshopping/woocommerce.php
+++ wp-content/themes/kenshopping/woocommerce.php
@@ -10,11 +10,15 @@
 
 get_header();?>
 <div id="primary" class="container-fluid">
-		<main id="main" class="row" role="main">
-		<?php woocommerce_content();?>
+	<main id="main" class="row" role="main">
+		<section class="col-xs-12 col-md-9 col-lg-9">
+			<?php woocommerce_content();?>
+		</section><!--.col-xs-12col-md-9col-g-9-->
+		<section class="col-xs-12 col-md-3 col-lg-3">
+			<?php get_sidebar();?>
+		</section><!--.col-xs-12col-md-3col-g-3-->
 	</main><!--#main-->
 </div><!--#primary .container-fluid-->
 
 <?php
-get_sidebar();
 get_footer();
