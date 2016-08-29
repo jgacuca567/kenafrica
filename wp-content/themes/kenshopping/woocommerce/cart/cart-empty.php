1a8a58af600e23b596a4386028dc6184f68c667e
James Van Waza <jgacuca@gmail.com>
2016-08-29 16:36:32 -0400

    PSR-4 Format

--- wp-content/themes/kenshopping/woocommerce/cart/cart-empty.php
+++ wp-content/themes/kenshopping/woocommerce/cart/cart-empty.php
@@ -16,7 +16,7 @@
  * @version 2.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly
 }
 
@@ -25,15 +25,15 @@ wc_print_notices();
 ?>
 
 <p class="cart-empty">
-	<?php _e( 'Your cart is currently empty.', 'woocommerce' ) ?>
+	<?php _e('Your cart is currently empty.', 'woocommerce')?>
 </p>
 
-<?php do_action( 'woocommerce_cart_is_empty' ); ?>
+<?php do_action('woocommerce_cart_is_empty');?>
 
-<?php if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
+<?php if (wc_get_page_id('shop') > 0): ?>
 	<p class="return-to-shop">
-		<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-			<?php _e( 'Return To Shop', 'woocommerce' ) ?>
+		<a class="button wc-backward" href="<?php echo esc_url(apply_filters('woocommerce_return_to_shop_redirect', wc_get_page_permalink('shop'))); ?>">
+			<?php _e('Return To Shop', 'woocommerce')?>
 		</a>
 	</p>
-<?php endif; ?>
+<?php endif;?>
--- wp-content/themes/kenshopping/woocommerce/cart/cart-item-data.php
+++ wp-content/themes/kenshopping/woocommerce/cart/cart-item-data.php
@@ -15,13 +15,13 @@
  * @package 	WooCommerce/Templates
  * @version 	2.4.0
  */
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 ?>
 <dl class="variation">
-	<?php foreach ( $item_data as $data ) : ?>
-		<dt class="variation-<?php echo sanitize_html_class( $data['key'] ); ?>"><?php echo wp_kses_post( $data['key'] ); ?>:</dt>
-		<dd class="variation-<?php echo sanitize_html_class( $data['key'] ); ?>"><?php echo wp_kses_post( wpautop( $data['display'] ) ); ?></dd>
-	<?php endforeach; ?>
+	<?php foreach ($item_data as $data): ?>
+		<dt class="variation-<?php echo sanitize_html_class($data['key']); ?>"><?php echo wp_kses_post($data['key']); ?>:</dt>
+		<dd class="variation-<?php echo sanitize_html_class($data['key']); ?>"><?php echo wp_kses_post(wpautop($data['display'])); ?></dd>
+	<?php endforeach;?>
 </dl>
