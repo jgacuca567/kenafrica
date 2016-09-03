d9d5113c5a9d6144ad7044eec5083dee2267cd3d
James Van Waza <jgacuca@gmail.com>
2016-09-03 19:53:25 -0400

    Added the btn class

--- wp-content/themes/kenshopping/woocommerce/loop/add-to-cart.php
+++ wp-content/themes/kenshopping/woocommerce/loop/add-to-cart.php
@@ -28,7 +28,7 @@ echo apply_filters('woocommerce_loop_add_to_cart_link',
 		esc_attr(isset($quantity) ? $quantity : 1),
 		esc_attr($product->id),
 		esc_attr($product->get_sku()),
-		esc_attr(isset($class) ? $class : 'button'),
+		esc_attr(isset($class) ? $class : 'btn btn-secondary'),
 		esc_html($product->add_to_cart_text())
 	),
 	$product);
