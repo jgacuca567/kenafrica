3941d5b70af904f891c73cd805f3d2e498ebd009
James Van Waza <jgacuca@gmail.com>
2016-10-17 18:20:06 -0400

    Removed the comma

--- wp-content/themes/kenshopping/footer.php
+++ wp-content/themes/kenshopping/footer.php
@@ -7,7 +7,7 @@
 <footer class="navbar navbar-default navbar-fixed-bottom text-center">
 	<i class="fa fa-copyright"></i>
 	Powered by <a href="https://woocommerce.com/" target="_blank">WooCommerce </a> |
-	, <a href="https://wordpress.org" target="_blank">Wordpress </a> |
+	<a href="https://wordpress.org" target="_blank">Wordpress </a> |
 	      <a href="https://dosbranding.com" target="_blank">DosBranding </a>
 	    <?php echo the_date('Y'); ?>
 </footer>
